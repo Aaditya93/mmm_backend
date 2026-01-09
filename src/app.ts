329ba1e6372928eb8c572f755289ce2fecc1b09e
Aaditya Sunil Shewale <aaditya9320@gmail.com>
2026-01-09 13:26:03 +0530

    docker file added

--- src/app.ts
+++ src/app.ts
@@ -19,10 +19,8 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-// Routes
 app.use("/", routes);
 
-// Error handling
 app.use(errorHandler);
 
 export default app;
